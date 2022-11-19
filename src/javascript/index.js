82c8bf57c055946b33bb90d18479b1443fcdab83
andriivam <andriivlasiuk2@gmail.com>
2022-11-19 10:47:44 +0100

    add icon and weather info

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,6 @@
 import mapboxgl from "mapbox-gl";
 import * as dotenv from 'dotenv';
+const cloud = document.getElementById('cloud');
 
  const formAddress = document.getElementById("formAddress");
 // adding event listener to our form
@@ -11,7 +12,24 @@ import * as dotenv from 'dotenv';
         fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?proximity=ip&types=address&access_token=${process.env.MAPBOX_API_KEY}`)
         .then((response) => response.json())
         .then(data => {
-            console.log(data.features[0].center)
+            console.log(data.features[0])
+// fetch weather api to get weather by location 
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data.features[0].center[1]}&lon=${data.features[0].center[0]}&appid=06a618ce91f49c8e5b9ccabb5f01a732&units=metric`)
+        .then((response) => response.json())
+        .then(data => {
+            console.log(data)
+        cloud.addEventListener('click', (e) => {
+const div = document.createElement('div');
+formAddress.appendChild(div);
+const p = document.createElement('p');
+p.innerHTML ='Feels_like: ' + data.main.feels_like;
+const span = document.createElement('span');
+span.innerHTML ='Temp: ' + data.main.temp;
+div.appendChild(p);
+div.appendChild(span);
+
+            })
+        })
 // take part of code frm mapBox resources
         mapboxgl.accessToken =
         "pk.eyJ1IjoiYW5kcmlpdmFtIiwiYSI6ImNsYWZhYno0cjBlOXczb3FqeXF6bXkzMWYifQ.tdL3jabQ3Ig0DwFd0xNClw";
@@ -26,6 +44,9 @@ const map = new mapboxgl.Map({
     map.on("style.load", () => {
         map.setFog({}); // Set the default atmosphere style
     });
+    const pEl = document.createElement("p");
+pEl.innerHTML = 'lat: ' + data.features[0].center[0] + ' long: ' + data.features[0].center[1];
+formAddress.appendChild(pEl);
         })
 
 });
